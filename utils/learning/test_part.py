fd757bda7b26888ace55dc9b65ab3ce0b9dc0bef
snu-njc <noddol1@snu.ac.kr>
2023-08-13 20:44:40 +0000

    ready for reconstruction

--- utils/learning/test_part.py
+++ utils/learning/test_part.py
@@ -6,16 +6,48 @@ from utils.common.utils import save_reconstructions
 from utils.data.load_data import create_data_loaders
 from utils.model.varnet import VarNet
 
-def test(args, model, data_loader):
-    model.eval()
+import fastmri as fastmri
+import config_file
+import losses
+import sampling
+import sde_lib
+import controllable_generation
+from models import ddpm, ncsnv2, ncsnpp
+from models import utils as mutils
+from models.ema import ExponentialMovingAverage
+from sampling import (ReverseDiffusionPredictor, 
+                      LangevinCorrector)
+import time
+
+def test(args, score_model, pc_inpainter, data_loader):
     reconstructions = defaultdict(dict)
+    start_iter = start = time.perf_counter()
     
     with torch.no_grad():
         for (mask, kspace, _, _, fnames, slices) in data_loader:
+            
+            b, c, h, w, two = kspace.shape
+            assert two == 2
+            kspace = kspace.view(b*c, h, w, 2).permute(0, 3, 1, 2)
+            kspace = kspace[:, :, :, w//2 - 192 : w//2 + 192]
+            mask = mask.view(1, 1, 1, -1)
+            mask = mask[:, :, :, w//2 - 192 : w//2 + 192]
             kspace = kspace.cuda(non_blocking=True)
             mask = mask.cuda(non_blocking=True)
-            output = model(kspace, mask)
-
+            
+            output = pc_inpainter(score_model, kspace, mask)
+            output = output.permute(0, 2, 3, 1).view(b, c, h, 384, 2)
+            result = fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(output)), dim=1)
+            height = result.shape[-2]
+            width = result.shape[-1]
+            output = result[..., (height - 384) // 2 : 384 + (height - 384) // 2, (width - 384) // 2 : 384 + (width - 384) // 2]
+            
+            print(
+                f'Iter = [{count:4d}/{len(data_loader):4d}] '
+                f'Time = {time.perf_counter() - start_iter:.4f}s',
+            )
+            start_iter = time.perf_counter()
+            
             for i in range(output.shape[0]):
                 reconstructions[fnames[i]][int(slices[i])] = output[i].cpu().numpy()
 
@@ -31,16 +63,29 @@ def forward(args):
     device = torch.device(f'cuda:{args.GPU_NUM}' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(device)
     print ('Current cuda device ', torch.cuda.current_device())
-
-    model = VarNet(num_cascades=args.cascade, 
-                   chans=args.chans, 
-                   sens_chans=args.sens_chans)
-    model.to(device=device)
+    config = config_file.get_config()
     
-    checkpoint = torch.load(args.exp_dir / 'best_model.pt', map_location='cpu')
-    print(checkpoint['epoch'], checkpoint['best_val_loss'].item())
-    model.load_state_dict(checkpoint['model'])
+    score_model = mutils.create_model(config)
+    
+    checkpoint = torch.load(args.exp_dir / 'model1.pt', map_location='cpu') ###########################################################
+    print(checkpoint['epoch'])
+    score_model.load_state_dict(checkpoint['model'])
+    
+    sde = sde_lib.VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=config.model.num_scales)
+    sampling_eps = 1e-5
+    
+    predictor = ReverseDiffusionPredictor
+    corrector = LangevinCorrector
+    snr = 0.16
+    n_steps = 1
+    probability_flow = False
+    pc_inpainter = controllable_generation.get_pc_inpainter(sde, predictor, corrector,
+                                                            snr=snr,
+                                                            n_steps=n_steps,
+                                                            probability_flow=probability_flow,
+                                                            continuous=config.training.continuous,
+                                                            denoise=True)
     
     forward_loader = create_data_loaders(data_path = args.data_path, args = args, isforward = True)
-    reconstructions, inputs = test(args, model, forward_loader)
+    reconstructions, inputs = test(args, score_model, pc_inpainter, forward_loader)
     save_reconstructions(reconstructions, args.forward_dir, inputs=inputs)
--- utils/learning/train_part.py
+++ utils/learning/train_part.py
@@ -53,93 +53,21 @@ def train_epoch(args, epoch, train_step_fn, data_loader, state):
                 f'Time = {time.perf_counter() - start_iter:.4f}s',
             )
             start_iter = time.perf_counter()
+    
     total_loss = total_loss / len_loader
     return total_loss, time.perf_counter() - start_epoch
 
 
-def validate(args, score_model, pc_inpainter, data_loader):
-    reconstructions = defaultdict(dict)
-    targets = defaultdict(dict)
-    start_iter = start = time.perf_counter()
-
-    with torch.no_grad():
-        for iter, data in enumerate(data_loader):
-            mask, kspace, target, _, fnames, slices = data
-            
-            b, c, h, w, two = kspace.shape
-            assert two == 2
-            kspace = kspace.view(b*c, h, w, 2).permute(0, 3, 1, 2)
-            kspace = kspace[:, :, :, w//2 - 192 : w//2 + 192]
-            mask = mask.view(1, 1, 1, -1)
-            mask = mask[:, :, :, w//2 - 192 : w//2 + 192]
-            kspace = kspace.cuda(non_blocking=True)
-            mask = mask.cuda(non_blocking=True)
-            
-            output = pc_inpainter(score_model, kspace, mask)
-            output = output.permute(0, 2, 3, 1).view(b, c, h, 384, 2)
-            result = fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(output)), dim=1)
-            height = result.shape[-2]
-            width = result.shape[-1]
-            output = result[..., (height - 384) // 2 : 384 + (height - 384) // 2, (width - 384) // 2 : 384 + (width - 384) // 2]
-
-            print(
-                f'Iter = [{iter:4d}/{len(data_loader):4d}] '
-                f'Time = {time.perf_counter() - start_iter:.4f}s',
-            )
-            start_iter = time.perf_counter()
-
-            for i in range(output.shape[0]):
-                reconstructions[fnames[i]][int(slices[i])] = output[i].cpu().numpy()
-                targets[fnames[i]][int(slices[i])] = target[i].numpy()
-            if iter >= 10:
-                break
-
-    for fname in reconstructions:
-        reconstructions[fname] = np.stack(
-            [out for _, out in sorted(reconstructions[fname].items())]
-        )
-    for fname in targets:
-        targets[fname] = np.stack(
-            [out for _, out in sorted(targets[fname].items())]
-        )
-    metric_loss = sum([ssim_loss(targets[fname], reconstructions[fname]) for fname in reconstructions])
-    num_subjects = len(reconstructions)
-    return metric_loss, num_subjects, reconstructions, targets, None, time.perf_counter() - start
-
-
-def save_model(args, exp_dir, epoch, model, optimizer, best_val_loss, is_new_best):
+def save_model(args, exp_dir, epoch, state):
     torch.save(
         {
             'epoch': epoch,
             'args': args,
-            'model': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'best_val_loss': best_val_loss,
+            'model': state['model'].state_dict(),
             'exp_dir': exp_dir
         },
-        f=exp_dir / 'model.pt'
+        f=exp_dir / str('model'+str(epoch)+'.pt')
     )
-    if is_new_best:
-        shutil.copyfile(exp_dir / 'model.pt', exp_dir / 'best_model.pt')
-
-
-def download_model(url, fname):
-    response = requests.get(url, timeout=10, stream=True)
-
-    chunk_size = 8 * 1024 * 1024  # 8 MB chunks
-    total_size_in_bytes = int(response.headers.get("content-length", 0))
-    progress_bar = tqdm(
-        desc="Downloading state_dict",
-        total=total_size_in_bytes,
-        unit="iB",
-        unit_scale=True,
-    )
-
-    with open(fname, "wb") as fh:
-        for chunk in response.iter_content(chunk_size):
-            progress_bar.update(len(chunk))
-            fh.write(chunk)
-
 
         
 def train(args):
@@ -176,52 +104,30 @@ def train(args):
     snr = 0.16
     n_steps = 1
     probability_flow = False
-    pc_inpainter = controllable_generation.get_pc_inpainter(sde,
-                                                            predictor, corrector,
+    pc_inpainter = controllable_generation.get_pc_inpainter(sde, predictor, corrector,
                                                             snr=snr,
                                                             n_steps=n_steps,
                                                             probability_flow=probability_flow,
                                                             continuous=config.training.continuous,
                                                             denoise=True)
 
-    best_val_loss = 1.
     start_epoch = 0
 
     train_loader = create_data_loaders(data_path = args.data_path_train, args = args, shuffle=True)
-    val_loader = create_data_loaders(data_path = args.data_path_val, args = args)
+    val_loader = create_data_loaders(data_path = args.data_path_val, args = args, shuffle=True)
     
-    val_loss_log = np.empty((0, 2))
     for epoch in range(start_epoch, args.num_epochs):
         print(f'Epoch #{epoch:2d} ............... {args.net_name} ...............')
         
         train_loss, train_time = train_epoch(args, epoch, train_step_fn, train_loader, state)
-        val_loss, num_subjects, reconstructions, targets, inputs, val_time = validate(args, score_model, pc_inpainter, val_loader)
-        
-        val_loss_log = np.append(val_loss_log, np.array([[epoch, val_loss]]), axis=0)
-        file_path = os.path.join(args.val_loss_dir, "val_loss_log")
-        np.save(file_path, val_loss_log)
-        print(f"loss file saved! {file_path}")
+        # we train on validation set.
+        val_loss, val_time = train_epoch(args, epoch, train_step_fn, val_loader, state)
 
         train_loss = torch.tensor(train_loss).cuda(non_blocking=True)
         val_loss = torch.tensor(val_loss).cuda(non_blocking=True)
-        num_subjects = torch.tensor(num_subjects).cuda(non_blocking=True)
 
-
-        val_loss = val_loss / num_subjects
-
-        is_new_best = val_loss < best_val_loss
-        best_val_loss = min(best_val_loss, val_loss)
-
-#         save_model(args, args.exp_dir, epoch + 1, model, optimizer, best_val_loss, is_new_best)
+        save_model(args, args.exp_dir, epoch + 1, state)
         print(
             f'Epoch = [{epoch:4d}/{args.num_epochs:4d}] TrainLoss = {train_loss:.4g} '
-            f'ValLoss = {val_loss:.4g} TrainTime = {train_time:.4f}s ValTime = {val_time:.4f}s',
+            f'TrainLoss2 = {val_loss:.4g} TrainTime = {train_time:.4f}s TrainTime2 = {val_time:.4f}s',
         )
-
-        if is_new_best:
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NewRecord@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            start = time.perf_counter()
-            save_reconstructions(reconstructions, args.val_dir, targets=targets, inputs=inputs)
-            print(
-                f'ForwardTime = {time.perf_counter() - start:.4f}s',
-            )
